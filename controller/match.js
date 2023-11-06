c137221759cc1c7a2f217d8171a902f306a559c2
christopher.totanes <christopher.totanes@gmail.com>
2023-11-06 22:36:02 +0800

    Start matchById route and controller

--- controller/match.js
+++ controller/match.js
@@ -1,6 +1,10 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
 
+let gameId = [], 
+    awayTeam = [],
+    homeTeam = [];
+
 export async function getAllMatches (req, res){
     try {
         const axiosResponse = await axios.get("https://www.nba.com/games", { responseType: 'text' });
@@ -16,11 +20,16 @@ export async function getAllMatches (req, res){
         let games = [];
         for (let game of scriptContent.props.pageProps.gameCardFeed.modules[0].cards){
             games.push(game)
+            gameId.push(game.cardData.gameId)
+            awayTeam.push(game.cardData.awayTeam.teamTricode.toLowerCase())
+            homeTeam.push(game.cardData.homeTeam.teamTricode.toLowerCase())
         };
         for (let headline of scriptContent.props.pageProps.headlines.headlines.items){
             headlines.push(headline)
         };
-        
+        console.log(gameId);
+        console.log(awayTeam);
+        console.log(homeTeam);
         console.log(`Axios and Cheerio request successful. Rendering with values: ${scriptContent}`);
         return res.render("index.ejs", {
             games: games,
@@ -32,4 +41,10 @@ export async function getAllMatches (req, res){
     }
 };
 
+// export async function getMatchByGameId (req, res){
+//     try {
+//         const axiosResponse = await axios.get("https://www.nba.com/games", { responseType: 'text' });
+//         console.log('Axios request successful');
+//     }
+// }
 export default getAllMatches;
--- route/match.js
+++ route/match.js
@@ -3,5 +3,6 @@ import matchController from '../controller/match.js';
 const router = express.Router();
 
 router.get("/", matchController);
+router.get("/:gameId", matchController)
 
 export default router
